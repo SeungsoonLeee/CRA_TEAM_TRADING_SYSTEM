91fff853dc7b9288b1469fcda7a04c8e84937452
hjjun <hjjun@mail.com>
2024-05-21 15:48:42 +0900

    add tests

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -29,6 +29,43 @@ TEST_F(TradingTest, LoginTest) {
 	system.login("ABC", "123");
 }
 
-TEST_F(TradingTest, BuyTest) {
+TEST_F(TradingTest, CurrentPriceTest) {
+	// 현재가를 확인하는 기능, 3번 호출하여 확인
+	EXPECT_CALL(mock, currentPrice("1234", 1))
+		.Times(3);
+	
+	// 코드구현
+
+}
+
+TEST_F(TradingTest, buyNiceTiming) {
+	// 가격이 올라가는 추세라면 매수
+	// 3번 연속 가격이 오르면 올라가는 추세라고 생각
+	// StockCode:1234  /  매수량:5  /  매수금액:최대금액
+	EXPECT_CALL(mock, currentPrice("1234", 1))
+		.WillOnce(Return(50))
+		.WillOnce(Return(100))
+		.WillOnce(Return(150));
+
+	EXPECT_CALL(mock, buy("1234", 5, 150))
+		.Times(1);
+
+	// 코드구현
+
+}
+
+TEST_F(TradingTest, sellNiceTiming) {
+	// 가격이 내려가는 추세라면 매도
+	// 3번 연속 가격이 내려가면 내려가는 추세라고 생각
+	// StockCode:1234  /  매도량:5  /  매수금액:최소금액
+	EXPECT_CALL(mock, currentPrice("1234", 1))
+		.WillOnce(Return(150))
+		.WillOnce(Return(100))
+		.WillOnce(Return(50));
+
+	EXPECT_CALL(mock, sell("1234", 5, 50))
+		.Times(1);
+
+	// 코드구현
 
 }
